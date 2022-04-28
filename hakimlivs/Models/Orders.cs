5edfac889bed2d04ef70e8e655cb1f76b05d658e
JonasMoss <jonasmoss@gmail.com>
2022-04-28 15:51:57 +0200

    Skrivit klasserna

---
+++ hakimlivs/Models/Orders.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hakimlivs.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
