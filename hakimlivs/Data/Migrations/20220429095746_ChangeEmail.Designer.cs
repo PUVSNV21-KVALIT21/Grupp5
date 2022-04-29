3d1151bdf5862b011acb1f080b6e5c6f52189bd3
leojus <jusufi93@hotmail.com>
2022-04-29 13:12:58 +0200

    Fixade till klasser och databasen.

--- hakimlivs/Data/Migrations/20220429095746_ChangeEmail.Designer.cs
+++ hakimlivs/Data/Migrations/20220429095746_ChangeEmail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using hakimlivs.Data;
 
@@ -11,9 +12,10 @@ using hakimlivs.Data;
 namespace hakimlivs.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220429095746_ChangeEmail")]
+    partial class ChangeEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- hakimlivs/Models/Order.cs
+++ hakimlivs/Models/Order.cs
@@ -5,9 +5,12 @@ namespace hakimlivs.Models
     public class Order
     {
         public int Id { get; set; }
+
         [Required]
         public string UserId { get; set; }
+
         public User User { get; set; }
+
         [Required]
         public DateTime Date { get; set; }
     }
