fd8f6cb6a2a12e7614cd9272925ad0736e0b9d0a
leojus <jusufi93@hotmail.com>
2022-05-06 14:49:03 +0200

    Add-Cart

---
+++ hakimlivs/Migrations/20220506124658_AddedCart.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hakimlivs.Migrations
+{
+    public partial class AddedCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                schema: "Identity",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cart",
+                schema: "Identity");
+        }
+    }
+}
