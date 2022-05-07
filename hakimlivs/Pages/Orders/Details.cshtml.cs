2237d4c81bf1aedd813c916ab18f6f0739d3a9f6
JonasMoss <jonasmoss@gmail.com>
2022-05-07 21:40:59 +0200

    Lagt till adminpanel, lite CRUD sidor etc

---
+++ hakimlivs/Pages/Orders/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using hakimlivs.Data;
+using hakimlivs.Models;
+
+namespace hakimlivs.Pages.Orders
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly hakimlivs.Data.ApplicationDbContext _context;
+
+        public DetailsModel(hakimlivs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Order Order { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order = await _context.Orders
+                .Include(o => o.User).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
