baf4e2e0e68ff488711aa95d285ba7b73aa291ef
leojus <jusufi93@hotmail.com>
2022-05-13 10:31:31 +0200

    Alert-Login

--- hakimlivs/wwwroot/js/site.js
+++ hakimlivs/wwwroot/js/site.js
@@ -1,6 +1,16 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function showMessage(item) {
-    alert(item + "Added to cart.");
+function clearCart() {
+    alert("Varukorg rensad.");
+}
+
+function orderPlaced() {
+    alert("Tack för din beställning.");
+}
+
+const form = document.getElementById("addForm");
+form.onsubmit = event => {
+    console.log("Hej");
+    event.preventDefault();
 }
