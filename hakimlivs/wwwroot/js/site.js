bbf2438a0ae0089b5f4ebe035c21d3a435f2d199
leojus <jusufi93@hotmail.com>
2022-05-13 10:44:37 +0200

    Fixat "popup" samt länk till inloggning.

--- hakimlivs/wwwroot/js/site.js
+++ hakimlivs/wwwroot/js/site.js
@@ -1,6 +1,10 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function showMessage(item) {
-    alert(item + "Added to cart.");
+function placeOrder() {
+    alert("Tack för din beställning.");
+}
+
+function clearCart() {
+    alert("Varukorgen är nu tom.")
 }
