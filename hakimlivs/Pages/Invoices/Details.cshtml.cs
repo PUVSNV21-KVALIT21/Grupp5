976adcebdfc9f3f96e143ed9638a0697f7625e71
Jorgen-son-of-karl <75936756+Jorgen-son-of-karl@users.noreply.github.com>
2022-05-13 11:34:19 +0200

    spara pdf

--- hakimlivs/Pages/Invoices/Details.cshtml.cs
+++ hakimlivs/Pages/Invoices/Details.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using hakimlivs.Data;
 using hakimlivs.Models;
+using ceTe.DynamicPDF.Conversion;
+using System.IO;
 
 namespace hakimlivs.Pages.Invoices
 {
@@ -39,6 +41,30 @@ namespace hakimlivs.Pages.Invoices
                 return NotFound();
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPost(int id)
+        {
+            string url = Request.Host.ToString();
+            HtmlConversionOptions options = new HtmlConversionOptions(false);
+
+            HtmlConverter htmlConverter = new HtmlConverter(new Uri(@"https://"+ url +"/Invoices/Details/" + id), options);
+            string folderPath = @"C:\faktura\";
+            Invoice = await _context.Invoices.FirstOrDefaultAsync(m => m.Id == id);
+            User = await _context.Users.FirstOrDefaultAsync(m => m.Id == Invoice.UserId);
+
+            if (Directory.Exists(folderPath))
+            {
+                htmlConverter.Convert(folderPath + User.Email + Invoice.PrintDate.ToString("yy/MM/dd") + ".pdf");
+            }
+
+            else
+            {
+                Directory.CreateDirectory(folderPath);
+                htmlConverter.Convert(folderPath + User.Email + Invoice.PrintDate.ToString("yy/MM/dd") + ".pdf");
+            }
+            
+            return RedirectToPage();
         }
     }
 }
